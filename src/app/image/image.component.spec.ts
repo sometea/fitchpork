1208f33dd317038374a7593c9bb608ff967a7a95
David Dasenbrook <david.dasenbrook@tngtech.com>
2017-10-02 19:44:56 +0200

    fix image storage and component test

--- src/app/image/image.component.spec.ts
+++ src/app/image/image.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageComponent } from './image.component';
+import { ImagesStorageService } from '../images-storage.service';
 
 describe('ImageComponent', () => {
   let component: ImageComponent;
   let fixture: ComponentFixture<ImageComponent>;
 
+  const ImagesStorageServiceStub = { };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageComponent ]
+      declarations: [ ImageComponent ],
+      providers: [
+        { provide: ImagesStorageService, useValue: ImagesStorageServiceStub },
+      ]
     })
     .compileComponents();
   }));
--- src/app/images-storage.service.spec.ts
+++ src/app/images-storage.service.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ImagesStorageService } from './images-storage.service';
+import { FirebaseApp } from 'angularfire2';
 
 describe('ImagesStorageService', () => {
+  const FirebaseAppStub = { 
+    storage: jasmine.createSpy('storage'),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ImagesStorageService]
+      providers: [
+        ImagesStorageService,
+        { provide: FirebaseApp, useValue: FirebaseAppStub }
+      ],
     });
   });
 
