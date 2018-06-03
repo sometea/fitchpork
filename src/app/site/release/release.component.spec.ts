fe5f5cf5b452ae1a12ee8f16c232b0f0b086eb8c
David Dasenbrook <david.dasenbrook@gmail.com>
2018-06-03 14:44:59 +0200

    fix link style and fix component tests

--- src/app/site/release/release.component.spec.ts
+++ src/app/site/release/release.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 import { ReleaseComponent } from './release.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ArticlesService } from '../../articles.service';
+import { Article, ArticleType } from '../../admin/edit-article/article';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ReleaseComponent', () => {
   let component: ReleaseComponent;
   let fixture: ComponentFixture<ReleaseComponent>;
 
+  const testArticle: Article = {
+    title: '', text: '', date: '', type: ArticleType.News, thumbnail: ''
+  };
+
+  const articlesServiceStub = {
+    get: jasmine.createSpy('get').and.returnValue(Observable.of(testArticle)),
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReleaseComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ ReleaseComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: ArticlesService, useValue: articlesServiceStub },
+      ],
     })
     .compileComponents();
   }));
--- src/app/site/releases/releases.component.spec.ts
+++ src/app/site/releases/releases.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 import { ReleasesComponent } from './releases.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ArticlesService } from '../../articles.service';
 
 describe('ReleasesComponent', () => {
   let component: ReleasesComponent;
   let fixture: ComponentFixture<ReleasesComponent>;
 
+  const articlesServiceStub = {
+    list: jasmine.createSpy('list').and.returnValue(Observable.of([]))
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReleasesComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ ReleasesComponent ],
+      providers: [
+        { provide: ArticlesService, useValue: articlesServiceStub },
+      ],
     })
     .compileComponents();
   }));
